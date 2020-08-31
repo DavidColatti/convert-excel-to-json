e98a91564e8361b9f4e4d123dff3a5dd7e8524fb
David Colatti <davidcolatti@gmail.com>
2020-08-31 10:36:08 -0400

    feat: excel to json

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import XLSX from "xlsx";
+import { make_cols } from "./makeCols";
+import { SheetJSFT } from "./types";
 
-function App() {
+const App = () => {
+  const [info, setInfo] = useState({ file: {}, data: [], cols: [] });
+
+  const handleChange = (e) => {
+    const files = e.target.files;
+
+    if (files && files[0]) {
+      setInfo({
+        ...info,
+        file: files[0],
+      });
+    }
+  };
+
+  const handleFile = () => {
+    /* Boilerplate to set up FileReader */
+    const reader = new FileReader();
+    const rABS = !!reader.readAsBinaryString;
+
+    reader.onload = (e) => {
+      /* Parse data */
+      const bstr = e.target.result;
+      const wb = XLSX.read(bstr, {
+        type: rABS ? "binary" : "array",
+        bookVBA: true,
+      });
+
+      /* Get first worksheet */
+      const wsname = wb.SheetNames[0];
+      const ws = wb.Sheets[wsname];
+
+      /* Convert array of arrays */
+      const newData = XLSX.utils.sheet_to_json(ws);
+
+      /* Update state */
+      setInfo({
+        ...info,
+        data: newData,
+        cols: make_cols(ws["!ref"]),
+      });
+    };
+
+    if (rABS) {
+      reader.readAsBinaryString(info.file);
+    } else {
+      reader.readAsArrayBuffer(info.file);
+    }
+
+    console.log(info.data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="file"
+        id="my_file_input"
+        accept={SheetJSFT}
+        onChange={handleChange}
+      />
+      <input type="submit" value="Process Triggers" onClick={handleFile} />
     </div>
   );
-}
+};
 
 export default App;
